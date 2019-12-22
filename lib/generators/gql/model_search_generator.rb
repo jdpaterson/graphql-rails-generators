278243547387e7c184984bf917542c0215d94f3c
J. Paterson <contact@jpatersondev.com>
2019-12-22 12:45:00 -0800

    inject field into query_type file

--- lib/generators/gql/model_search_generator.rb
+++ lib/generators/gql/model_search_generator.rb
@@ -6,6 +6,11 @@ module Gql
     argument :model_name, type: :string
 
     def search
+      inject_into_file(
+        "app/graphql/types/query_type.rb", 
+        "\t\tfield :#{model_name.downcase.pluralize}, resolver: Resolvers::#{model_name}Search \n", 
+        :after => "class QueryType < Types::BaseObject\n"
+      )
       file_name = "#{model_name.underscore}_search"
       @fields = map_model_types(model_name)
       template('model_search.rb', "app/graphql/resolvers/#{file_name}.rb")
